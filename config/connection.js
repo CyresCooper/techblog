05bc9267aa00904d3aad024dc9c65e2800138902
Cyres Cooper <95892453+CyresCooper@users.noreply.github.com>
2022-04-16 22:20:32 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'lo0.0.0.0',
+      host: '0.0.0.0',
       dialect: 'mysql',
       port: 3001
     });
